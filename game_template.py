0a4ab3c7396da22c66a9a080b4d0b3a6335ba44b
Trevon Wiggs <trevonwiggs@gmail.com>
2017-09-30 15:27:47 -0400

    template for starting a game

---
+++ game_template.py
@@ -0,0 +1,23 @@
+import pygame
+from random import randint
+import sys
+from pygame.locals import*
+
+pygame.init()
+
+size = 500,500
+screen = pygame.display.set_mode(size)
+background = pygame.image.load()
+screen.blit(background, [0,0])
+pygame.display.set_caption()
+
+
+
+# define objects here
+
+
+def game():
+    #write code here for game
+
+game()
+quit()
